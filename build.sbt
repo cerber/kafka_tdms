131b5655b95bec3d58345557edf1320acfbca863
Dmytro Nezhynskiy <d.nezhinsky@gmail.com>
2016-03-30 18:38:50 +0300

    Stream processing

--- build.sbt
+++ build.sbt
@@ -2,13 +2,15 @@ name := "kafka_tdms"
 
 version := "1.0"
 
-scalaVersion := "2.10.5"
+scalaVersion := "2.10.6"
 
 unmanagedBase := baseDirectory.value / "libs"
 
 unmanagedJars in Compile := (baseDirectory.value  ** "*.jar").classpath
 
 javaOptions in run += "-Djava.library.path=libs:lib/native/linux"
+
+ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
 
 libraryDependencies ++= Seq (
   "org.apache.kafka" %% "kafka" % "0.8.1"
@@ -19,6 +21,8 @@ libraryDependencies ++= Seq (
   "ch.qos.logback" % "logback-classic" % "1.1.6",
   "ch.qos.logback" % "logback-core" % "1.1.6",
   "org.slf4j" % "slf4j-api" % "1.7.19",
+  "org.json4s" %% "json4s-native" % "3.3.0",
+  "org.json4s" %% "json4s-jackson" % "3.3.0",
   "net.liftweb" %% "lift-json" % "2.6.3",
   "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 )
--- src/main/scala/TdmsReader.scala
+++ src/main/scala/TdmsReader.scala
@@ -1,8 +1,4 @@
 import java.io.File
-
-import net.liftweb.json._
-import net.liftweb.json.Serialization._
-import net.liftweb.json.JsonDSL._
 import tdms.{TdmsChannel, TdmsGroup, TdmsParser}
 
 /**
@@ -16,81 +12,42 @@ object TdmsReader {
 
     val fileName = getListOfFiles(args(0)).head.getAbsolutePath
 
-    println(convTdms2Json(fileName))
+    convTdms2Json(fileName, isVerbose = false)
 
   }
 
-  def test(fileName: String): Unit = {
-    println("FileName: " + fileName)
+  def convTdms2Json(fileName: String, isVerbose: Boolean = false): Unit = {
+
+    import org.json4s.JsonDSL._
+    import org.json4s.native.Serialization
+    import org.json4s.native.Serialization.write
+    import org.json4s.NoTypeHints
 
     val parser = new TdmsParser(fileName)
-
     if(parser.fileOpeningError == 1) {
-      println("Input error: please provide a valid *.tdms file name!")
-      sys.exit
-    }
-
-    parser.read(false)
-
-    val groupCount = parser.getGroupCount
-    printf("Number of groups: %d\n", groupCount)
-
-    for (i <- 0 until groupCount.toInt) {
-      val group = parser.getGroup(i)
-      val channelCount = group.getGroupSize
-      printf("Group %s has %d channels\n", group.getName, channelCount)
-
-      for (j <- 0 until channelCount.toInt) {
-        val channel = group.getChannel(j)
-        printf("  Channel %s (unit: %s) has %d values, type: %d properties: %s",
-          channel.getName,
-          channel.getUnit,
-          channel.getDataCount,
-          channel.getDataType,
-          channel.propertiesToString)
-        val data = channel.getDataVector
-        printf("\t% 10.2f ... % 10.2f\n", data.get(0), data.get((data.size - 1).toInt))
-      }
-    }
-  }
-
-  def convTdms2Json(fileName: String, isVerbose: Boolean = false): String = {
-    val parser = new TdmsParser(fileName)
-    if(parser.fileOpeningError == 1) {
-      return ""
+      return
     }
 
     parser.read(isVerbose)
-//    val groups = getTdmsGroups(parser)
-//    val channels = groups.map(g => getTdmsChannels(g))
-//    val data = channels.map(lc => lc.map(c => getTdmsDataVector(c)))
 
+    implicit val formats = Serialization.formats(NoTypeHints)
+    val output = new java.io.BufferedWriter(new java.io.FileWriter(new java.io.File("/tmp/test.json")))
 
-/*
-    val d = getTdmsGroups(parser).map(group => {
-      Group(group.getName, getTdmsChannels(group).map(channel => {
-        Channel(channel.getName, channel.getUnit, channel.getDataType.toInt, getTdmsDataVector(channel))
-      }))
-    })
-*/
-
-    val d = getTdmsGroups(parser).map(group => {
-      val groupName = group.getName
-      val lc = getTdmsChannels(group)
-      val c = lc.map(channel => {
-        val channelName = channel.getName
-        val channelUnit = channel.getUnit
-        val channelDataType = channel.getDataType.toInt
-        val dataVector = getTdmsDataVector(channel)
-        Channel(channelName, channelUnit, channelDataType, dataVector)
-      })
-      Group(groupName, c)
-    })
-
-
-    implicit val formats = DefaultFormats
-    val json = write(d)
-    pretty(render(json))
+    val res = getTdmsGroups(parser).foreach { g =>
+      getTdmsChannels(g).foreach { c =>
+        val json =
+        "group" ->
+          ("name" -> g.getName) ~
+            ("channels" ->
+              ("name" -> c.getName) ~
+                ("unit" -> c.getUnit) ~
+                ("dataType" -> c.getDataType) ~
+                ("data" -> getTdmsDataVector(c)))
+        write(json, output)
+      }
+    }
+    output.flush
+    output.close
   }
 
   def getTdmsGroups(parser: TdmsParser): List[TdmsGroup] = {
@@ -103,6 +60,7 @@ object TdmsReader {
 
   def getTdmsDataVector(channel: TdmsChannel): List[Double] = {
     val data = channel.getDataVector
+    printf("Processed channel: %s\n", channel.getName)
     for (i <- (0 until (data.size - 1).toInt).toList) yield data.get(i)
   }
 
@@ -115,6 +73,3 @@ object TdmsReader {
     }
   }
 }
-
-case class Group(name: String, channels: List[Channel])
-case class Channel(name: String, unit: String, dataType: Int, data: List[Double])
